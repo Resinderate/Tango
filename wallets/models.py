c3055537ce3cf4564071f170aa244720b4173479
Ronan Murphy <ronan_murfy@hotmail.com>
2017-09-01 22:28:59 +0100

    Moved models to the right folder.

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Wallet(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	balance = models.DecimalField(max_digits=10, decimal_places=2)
+	play_money = models.BooleanField(default=True)
+	currency = models.CharField(max_length=50) # Add choices here eventually.
