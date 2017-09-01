f44a5469f7446cf5bfe0bb52d239ff6c2eafdc7f
Ronan Murphy <ronan_murfy@hotmail.com>
2017-09-01 23:05:50 +0100

    Added transactions app + models.

---
+++ transactions/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from wallets.models import Wallet
+
+
+class Transaction(models.Model):
+	"""Something that brings the balance of a wallet up or down.
+
+	These transactions should be reflected in the wallets they point to as they are created.
+	Do it in the init or something.
+	"""
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	wallet = models.ForeignKey(Wallet)
+
+	class Meta:
+		abstract = True
+
+class UserTransaction(Transaction):
+	"""A transaction where the source of the amount is the user's external money source.
+
+	A deposit to the account, a withdrawl from the account
+	"""
+	# Bank account?, some sort of money source.
+	pass
+
+
+class StaffTransaction(Transaction):
+	"""A transaction where the source of the amount is from the platform.
+
+	From promotions, docking amounts from users..
+	"""
+	# Could have a FK to some sort of elaborated reason here.
+	reason = models.CharField(max_length=512)
+
+
+class GameTransaction(Transaction):
+	"""A transaction where the source of the amount is from a game.
+
+	A spin that loses/wins the player some money..
+	"""
+	stake = models.DecimalField(max_digits=10, decimal_places=2)
+	won = models.BooleanField()
+	# Could record the odds, in case it's not fixed.
