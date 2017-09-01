e6f4cf46be7dfc2342bf92a4584fba1d465b1cda
Ronan Murphy <ronan_murfy@hotmail.com>
2017-09-01 22:24:33 +0100

    Added wallets app.

---
+++ wallets/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class WalletsConfig(AppConfig):
+    name = 'wallets'
+
+
+class Wallet(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	balance = models.DecimalField(max_digits=10, decimal_places=2)
+	play_money = models.BooleanField(default=True)
+	currency = models.CharField(max_length=50) # Add choices here eventually.
